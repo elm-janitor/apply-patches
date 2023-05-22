ae54443d300c5d0127db5ed143a1e4740a363bbc
Marc Walter <walter.marc@outlook.com>
2023-05-22 21:20:26 +0200

    Extract helper functions

--- deno/cli.ts
+++ deno/cli.ts
@@ -39,6 +39,36 @@ async function printStatus(elmHomeDir: string, verbose: boolean) {
   }
 }
 
+async function installAllPatches(elmHomeDir: string, verbose: boolean) {
+  for (const pkg of Object.keys(knownPatches)) {
+    console.log("");
+    await installPatch({ elmHomeDir, pkg, verbose });
+  }
+}
+
+async function installPatches(
+  elmHomeDir: string,
+  pkgs: Array<string | number>,
+  verbose: boolean,
+) {
+  pkgs.forEach((pkg: string | number) => {
+    if (typeof pkg !== "string" || !knownPatches[pkg]) {
+      console.error(`I don't know how to patch package 'elm/${pkg}'.`);
+      console.error("Will quit now.");
+      Deno.exit(1);
+    }
+  });
+
+  for (const pkg of pkgs) {
+    console.log("");
+    await installPatch({
+      elmHomeDir,
+      pkg: pkg as string,
+      verbose,
+    });
+  }
+}
+
 const elmHomeDir = findElmHome();
 console.log(`Working with ELM_HOME '${elmHomeDir}'.`);
 
@@ -51,27 +81,9 @@ if (flags.help) {
 } else if (flags.status) {
   await printStatus(elmHomeDir, flags.verbose);
 } else if (flags.all) {
-  for (const pkg of Object.keys(knownPatches)) {
-    console.log("");
-    await installPatch({ elmHomeDir, pkg, verbose: flags.verbose });
-  }
+  await installAllPatches(elmHomeDir, flags.verbose);
 } else if (Array.isArray(flags._) && flags._.length > 0) {
-  flags._.forEach((pkg: string | number) => {
-    if (typeof pkg !== "string" || !knownPatches[pkg]) {
-      console.error(`I don't know how to patch package 'elm/${pkg}'.`);
-      console.error("Will quit now.");
-      Deno.exit(1);
-    }
-  });
-
-  for (const pkg of flags._) {
-    console.log("");
-    await installPatch({
-      elmHomeDir,
-      pkg: pkg as string,
-      verbose: flags.verbose,
-    });
-  }
+  await installPatches(elmHomeDir, flags._, flags.verbose);
 } else {
   printHelp();
 }
